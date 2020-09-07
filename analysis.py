1078b7e74d885e351b845e64318c11751d276758
A-Lohse <augustlohse@yahoo.dk>
2020-09-07 11:23:28 +0200

    added empty scripts

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 11:22:36 2020
+
+@author: augus
+"""
+
---
+++ dataclean.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 11:21:37 2020
+
+@author: august
+"""
+
